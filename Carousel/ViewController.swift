829f79ee8776bf82407a7696143037676e846c2d
27ugeen <27ugeen@gmail.com>
2022-04-13 09:12:29 +0300

    Initial Commit

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by GiN Eugene on 13/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
